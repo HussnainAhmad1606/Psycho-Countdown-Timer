77d9f38bb0a8e861450a5625b297a5d3c15ec8ce
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2022-08-01 23:48:41 +0500

    Font Customization, Timer Stop & Reset
    Add the ability for the user to change the font of the GUI. Now user can stop the time and also reset the time easily.

--- script.py
+++ script.py
@@ -9,8 +9,13 @@ root.resizable(0,0)
 
 
 # Font Name and Size to be used to show on timer
-fontName = "Book Antiqua"
-fontSize = 70
+fontName = StringVar()
+fontName.set("Book Antiqua")
+fontSize = IntVar()
+fontSize.set(50)
+
+btnFontSize = IntVar()
+btnFontSize.set(15)
 softwareVersion = "1.0"
 
 # Github Profile Link
@@ -19,12 +24,52 @@ githubLink = "https://github.com/HussnainAhmad1606/"
 # At started timer will be stop
 isTimeStarted = False
 
+# Available fonts on GUI
+fonts = ["Book Antiqua", "Stencil", "Rockwell", "Poppins", "New Athletic M54", "Molot", "Georgia", "Century Gothic"]
+
+loopFunction = None
+
+
 # Function to add 0 to the left of hours, minutes & seconds
 def addZero(value):
     if int(value) < 10:
         return f"0{int(value)}"
     else:
         return value
+
+
+def changeFont():
+    font = Tk()
+    font.title("Change Font")
+    font.geometry("300x300")
+    fontLabel = Label(font, text="Change Font", font=(fontName.get(), 12))
+    fontLabel.pack(padx=10, pady=10)
+    fontBox = Listbox(font, selectmode=SINGLE)
+    for index,value in enumerate(fonts):
+        fontBox.insert(index+1, value)
+    fontBox.pack()
+
+    def setFont():
+        global fontName
+        fontName.set(fontBox.get(ACTIVE))
+        hoursLabel.configure(font=(fontName.get(), fontSize.get()))
+        colon1.configure(font=(fontName.get(), fontSize.get()))
+        colon2.configure(font=(fontName.get(), fontSize.get()))
+        minutesLabel.configure(font=(fontName.get(), fontSize.get()))
+        secondsLabel.configure(font=(fontName.get(), fontSize.get()))
+        setTimerBtn.configure(font=(fontName.get(), btnFontSize.get()))
+        startTimerBtn.configure(font=(fontName.get(), btnFontSize.get()))
+        stopTimerBtn.configure(font=(fontName.get(), btnFontSize.get()))
+        resetTimerBtn.configure(font=(fontName.get(), btnFontSize.get()))
+        showinfo("Font Changed", f"Font Changed to {fontName.get()}")
+
+        font.destroy()
+
+
+    setFontBtn = Button(font, text="Set Font", command=setFont)
+    setFontBtn.pack(padx=10, pady=10)
+
+    font.mainloop()
 
 # Function that will change the count down timer
 def changeTheTime():
@@ -52,26 +97,37 @@ def startTimer():
     if (int(hours.get()) == 0 and int(minutes.get()) == 0 and int(seconds.get()) == 0):
         showwarning("Time Up", "Time is up. Set the time again for next counting")
     else:
+        startTimerBtn["state"] = DISABLED
+        stopTimerBtn["state"] = NORMAL
         if isTimeStarted:
             changeTheTime()
-            root.after(1000, startTimer)
+            global loopFunction
+            loopFunction = root.after(1000, startTimer)
 
 
 # Stop the timer
 def stopTimer():
     global isTimeStarted
+    global loopFunction
     isTimeStarted = False
+    root.after_cancel(loopFunction)
+    startTimerBtn["state"] = NORMAL
+    stopTimerBtn["state"] = DISABLED
 
 # Reset the timer
 def resetTimer():
-    hours.set(0)
-    minutes.set(0)
-    seconds.set(0)
+    startTimerBtn["state"] = NORMAL
+    stopTimerBtn["state"] = DISABLED
+    if loopFunction is not None:
+        root.after_cancel(loopFunction)
+    hours.set("00")
+    minutes.set("00")
+    seconds.set("00")
 
 
 # Info About the software
 def aboutSoftware():
-    about = f"Software: Psycho Timer\nVersion: {softwareVersion}\nProgrammer: Psycho Coder"
+    about = f"Software: Psycho Timer\nVersion: {softwareVersion}\nProgrammer: Psycho Coder\n Font Name: {fontName.get()}"
     showinfo("About", about)
 
 # Visiting the github profile when user clicks on visit website on about menu
@@ -123,37 +179,42 @@ seconds.set("05")
 
 # Main Window
 watchFrame = Frame(root)
-hoursLabel = Label(watchFrame, textvariable=hours, font=(fontName, fontSize))
+hoursLabel = Label(watchFrame, textvariable=hours, font=(fontName.get(),fontSize.get()))
 hoursLabel.pack(side=LEFT)
-colon1 = Label(watchFrame, text=":", font=(fontName, fontSize))
+colon1 = Label(watchFrame, text=":", font=(fontName.get(), fontSize.get()))
 colon1.pack(side=LEFT)
-minutesLabel = Label(watchFrame, textvariable=minutes, font=(fontName, fontSize))
+minutesLabel = Label(watchFrame, textvariable=minutes, font=(fontName.get(), fontSize.get()))
 minutesLabel.pack(side=LEFT)
-colon1 = Label(watchFrame, text=":", font=(fontName, fontSize))
-colon1.pack(side=LEFT)
-secondsLabel = Label(watchFrame, textvariable=seconds, font=(fontName, fontSize))
+colon2 = Label(watchFrame, text=":", font=(fontName.get(), fontSize.get()))
+colon2.pack(side=LEFT)
+secondsLabel = Label(watchFrame, textvariable=seconds, font=(fontName.get(), fontSize.get()))
 secondsLabel.pack(side=LEFT)
+
 watchFrame.pack(side=TOP)
 
 buttonsFrame = Frame(root)
 
-setTimerBtn = Button(buttonsFrame, text="Set Timer", command=setTimer)
-setTimerBtn.pack(side=LEFT, padx=50, pady=50)
+setTimerBtn = Button(buttonsFrame, text="Set Timer", command=setTimer, font=(fontName.get(), btnFontSize.get()))
+setTimerBtn.pack(side=LEFT, padx=20, pady=20)
 
-setTimerBtn = Button(buttonsFrame, text="Start", command=startTimer)
-setTimerBtn.pack(side=LEFT, padx=50, pady=50)
+startTimerBtn = Button(buttonsFrame, text="Start", command=startTimer, font=(fontName.get(), btnFontSize.get()))
+startTimerBtn.pack(side=LEFT, padx=20, pady=20)
 
-setTimerBtn = Button(buttonsFrame, text="Stop", command=stopTimer)
-setTimerBtn.pack(side=LEFT, padx=50, pady=50)
+stopTimerBtn = Button(buttonsFrame, text="Stop", command=stopTimer, font=(fontName.get(), btnFontSize.get()))
+stopTimerBtn.pack(side=LEFT, padx=20, pady=20)
 
-setTimerBtn = Button(buttonsFrame, text="Reset", command=resetTimer)
-setTimerBtn.pack(side=LEFT, padx=50, pady=50)
+resetTimerBtn = Button(buttonsFrame, text="Reset", command=resetTimer, font=(fontName.get(), btnFontSize.get()))
+resetTimerBtn.pack(side=LEFT, padx=20, pady=20)
 
 buttonsFrame.pack()
 
 # Main Menu
 mainMenu = Menu(root)
 
+preferencesMenu = Menu(root, tearoff=0)
+preferencesMenu.add_command(label="Fonts", command=changeFont)
+
+mainMenu.add_cascade(label="Preferences", menu=preferencesMenu)
 aboutMenu = Menu(mainMenu, tearoff=0)
 aboutMenu.add_command(label="Quit", command=root.destroy)
 aboutMenu.add_separator()
@@ -161,5 +222,6 @@ aboutMenu.add_command(label="About Software", command=aboutSoftware)
 aboutMenu.add_separator()
 aboutMenu.add_command(label="Visit Website", command=visitWebsite)
 mainMenu.add_cascade(label="About", menu=aboutMenu)
+
 root.config(menu=mainMenu)
 root.mainloop()
